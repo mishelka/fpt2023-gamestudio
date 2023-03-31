96d763ee6ce7c184fb49487605ed6b5b805fe9ac
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-03-31 15:11:17 +0200

    cors config pre react mines app

--- src/main/java/sk/tuke/gamestudio/server/GameStudioServer.java
+++ src/main/java/sk/tuke/gamestudio/server/GameStudioServer.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sk.tuke.gamestudio.common.service.*;
 import sk.tuke.gamestudio.server.service.*;
 
@@ -27,5 +29,20 @@ public class GameStudioServer {
     @Bean
     public RatingService getRatingService() {
         return new RatingServiceJPA();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        String[] allowDomains = new String[2];
+        allowDomains[0] = "http://localhost:3000";
+        allowDomains[1] = "http://localhost:8080";
+
+        System.out.println("CORS configuration....");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowDomains);
+            }
+        };
     }
 }
